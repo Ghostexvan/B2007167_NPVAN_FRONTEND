33ca01f98902a13a6918c015ea17a8061052a2a6
Nguyen Phuc Vinh An B2007167 <anb2007167@student.ctu.edu.vn>
2023-10-23 22:27:24 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script>
-  export default{
+  import AppHeader from "@/components/AppHeader.vue";
 
-  }
+  export default{
+    components: {
+      AppHeader,
+    },
+  };
 </script>
 
 <template>
-  <h1>Hello, Vue.js!</h1>
+  <div>
+    <AppHeader />
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style>
+  #app{
+    height: 100vh;
+  }
   .page {
     max-width: 400px;
     margin: auto;
